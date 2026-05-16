e74862398f126c227b3b840756e96f3a8e85d23a
Adam Petersen <pet23079@byui.edu>
2026-05-15 22:46:01 -0600

    Add initial implementation of photo gallery with modal viewer
    - Created index.html for the gallery layout including header, main content, and footer.
    - Added script.js to handle modal functionality for image viewing.
    - Implemented style.css for styling the gallery, modal, and responsive design.

---
+++ W04/prove/script.js
@@ -0,0 +1,31 @@
+const gallery = document.querySelector('.cool-pics');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    // Store image filepath in imageSrc
+    let imageSrc = e.target.src;
+
+    // Replace filepath's sm with full to display the high-res version of a given image,
+    // and store the new high-res filepath in modalImage
+    modalImage.src = imageSrc.replace("-sm", "-full");
+    console.log(imageSrc);
+
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
